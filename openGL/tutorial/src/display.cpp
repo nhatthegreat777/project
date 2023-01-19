439e2447b43f631408d573eed8110860e960c343
nhatthegreat777 <nhatvan1561@gmail.com>
2023-01-18 20:30:13 -0600

    comment

--- openGL/tutorial/src/display.cpp
+++ openGL/tutorial/src/display.cpp
@@ -10,14 +10,3 @@ Display::~Display()
   //dtor
 }
 
-Display::Display(const Display& other)
-{
-  //copy ctor
-}
-
-Displays& Display::operator=(const Display& rhs)
-{
-  if (this == &rhs) return *this; // handle self assigment
-  //assignment operator
-  return *this;
-}
--- openGL/tutorial/src/display.h
+++ openGL/tutorial/src/display.h
@@ -1,15 +1,16 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <string>
 class Display
 {
   public:
-    Display();
+    Display(int width, int height, const std::string& title);
     virtual ~Display(); // overridden by derived class
-    Display(const Display& other);
-    Display& operator=(const Display& other);
   protected:
   private:
+    Display(const Display& other){}
+    Display& operator=(const Display& other){}
 };
 
 #endif // DISPLAY_H
